7f3c0982cb1ba4dff8d49eb9574326ca1a331de3
mldn <liuwei99999819@163.com>
2017-10-19 15:34:46 +0800

    这就是一个sb

--- src/com/qh/test/TestMain.java
+++ src/com/qh/test/TestMain.java
@@ -3,7 +3,7 @@ package com.qh.test;
 public class TestMain {
 	public static void main(String[] args) {
 		System.out.println("北京");
-		System.out.println("欢迎你");
+		System.out.println("欢迎你小金子");
 		
 		
 		
