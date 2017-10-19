8941c3e39e7756cf1bc6f3fef465cead9159fe00
mldn <liuwei99999819@163.com>
2017-10-19 15:31:42 +0800

    这是test

--- src/com/qh/test/TestMain.java
+++ src/com/qh/test/TestMain.java
@@ -3,7 +3,9 @@ package com.qh.test;
 public class TestMain {
 	public static void main(String[] args) {
 		System.out.println("±±¾©");
-		System.out.println("");
+		System.out.println("»¶Ó­Äã");
+		
+		
 		
 		
 		
